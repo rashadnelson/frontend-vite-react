4aee42a11caf8a56799bbedd8852c0f89a0bf0ed
Rashad Nelson <rashadnelson@gmail.com>
2022-03-19 13:29:42 -0400

    First commit of Vite-React Project

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import './App.css'
+
+function App() {
+
+
+  return (
+    <div className="App">
+      Hi, this is your Vite / React App.
+    </div>
+  )
+}
+
+export default App
